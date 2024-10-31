fe19ce1f4b910e66d5a4011ae2a7ee3e77886222
ANTOINE <ANT.FOUB@GMAIL.COM>
2024-10-31 16:24:19 +0100

    exo fini

---
+++ index.js
@@ -0,0 +1,16 @@
+import dotenv from 'dotenv';
+import cowsay from 'cowsay';
+
+
+dotenv.config();
+
+
+const name = process.env.NAME;
+const campus = process.env.CAMPUS;
+
+
+console.log(cowsay.say({
+    text: `Hello, my name is ${name} and I study at ${campus}!`,
+    e: "oO",
+    T: "U "
+}));
